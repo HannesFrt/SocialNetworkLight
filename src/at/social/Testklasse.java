ad677f7b6f669cee9a9efaebf84c1858807fe7e9
hanne <hanne@DESKTOP-T1FCK4D>
2017-03-15 08:57:48 +0100

    First klass

---
+++ src/at/social/Testklasse.java
@@ -0,0 +1,11 @@
+package at.social;
+
+public class Testklasse {
+	public static void main(String[] args) {
+		//Comment moin
+		System.out.println("Test");
+		System.out.println("Test 2");
+		System.out.println("");
+	}
+
+}
